efb7e7bc188fdfd2d4f6080785c86fdd4b19728f
PARK <trongofficel@gmail.com>
2025-09-22 08:58:34 +0700

    update environmet

--- GreenCharger/src/environments/environment.prod.ts
+++ GreenCharger/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://https://greencharger-api-b8gce4fsdhchabdp.southeastasia-01.azurewebsites.net', // URL production API
+  apiUrl: 'https://greencharger-api-b8gce4fsdhchabdp.southeastasia-01.azurewebsites.net/api', // URL production API
   apiTimeoutMs: 30000,
   appName: 'GreenCharger Admin'
 };
